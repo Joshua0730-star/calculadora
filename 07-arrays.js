07219115408b1f550d9c26fe4e6d647cfa3d717f
josue marchena <josuemarchena0730@gmail.com>
2024-10-28 20:25:36 -0500

    añadiendo clase de arrays

---
+++ 07-arrays.js
@@ -0,0 +1,35 @@
+/* -- ¿ QUE ES UN ARRAY O ARREGLO --
+(coleccion) 
+
+
+--estos comienzan desde el numero 0
+*/
+
+
+// Definiendo un Array 
+let animales=['chanchito', 'caballo'];
+
+console.log(animales);
+console.log(animales[0]);
+// asignando un dato en el indice 2 , que no estaba desde el principio
+animales[2]= 'dragon';
+console.log(animales);
+
+animales[10]='pez';
+console.log(animales[7]);
+console.log( typeof animales);
+console.log( animales.length); // esta propiedad es  para averiguar la longitud en este caso del array
+
+
+
+let numbers = [1,2,3,4,5,6,7,8,9];
+console.log(numbers);
+numbers[10]= 'josue';
+console.log(numbers[10]);
+
+let notas = [1,2,3,4,5];
+console.log(notas[2]);
+
+
+
+
